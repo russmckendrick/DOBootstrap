75f99c8348b9f9bd19b63351dde8563da0fa2a37
Russ McKendrick <russ@mckendrick.io>
2015-07-12 10:35:29 +0100

    Making pretty

--- do-bootstrap.sh
+++ do-bootstrap.sh
@@ -1,39 +1,38 @@
-## Enable SWAP
-fallocate -l 4G /swapfile  
-chmod 600 /swapfile  
-mkswap /swapfile  
-swapon /swapfile  
+echo "======================================================================================"
+echo "Digital Ocean CentOS7 Bootstrap"
+echo ""
+echo ""
+echo "======================================================================================"
+echo ""
+echo "=> Enabling SWAP ..."
+fallocate -l 4G /swapfile > /dev/null 2>&1
+chmod 600 /swapfile > /dev/null 2>&1
+mkswap /swapfile > /dev/null 2>&1
+swapon /swapfile  > /dev/null 2>&1
 echo "/swapfile none swap sw 0 0" >> /etc/fstab  
 echo "## Make Swap betterer" >> /etc/sysctl.conf  
 echo "vm.swappiness = 10" >> /etc/sysctl.conf  
 echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
- 
+echo "=> Done!"
+echo "=> Installing EPEL, fail2ban, ntp, git and vim-enhanced ..."
 ## Install and configure the basics
-yum install -y epel-release deltarpm && yum install -y fail2ban vim-enhanced ntpd git
+yum install -y epel-release deltarpm > /dev/null 2>&1
+yum install -y fail2ban vim-enhanced ntp git > /dev/null 2>&1
 cat >> /etc/fail2ban/jail.local << FAIL2BAN_CONFIG 
 [sshd]
 enabled = true
 maxretry = 3
 bantime  = 86400
 FAIL2BAN_CONFIG
- 
-## Set the timezone
+echo "=> Done!"
+echo "=> Setting the timezone to Europe/London ..."
 timedatectl set-timezone Europe/London
- 
-## Run an update
-yum update -y
- 
-## Set the services to start on boot
-systemctl enable firewalld && systemctl enable fail2ban && systemctl enable ntp
- 
-## Prompt for a reboot
-echo ""
-read -r -p "DO stuff applied, do you want to reboot? [y/N] " response
-case $response in
-    [yY][eE][sS]|[yY]) 
-        reboot
-        ;;
-    *)
-        exit
-        ;;
-esac
+echo "=> Done!"
+echo "=> Running a yum update ..."
+yum update -y > /dev/null 2>&1
+echo "=> Done!"
+echo "=> Setting the installed services to start on boot ..."
+systemctl enable firewalld > /dev/null 2>&1
+systemctl enable fail2ban > /dev/null 2>&1
+systemctl enable ntpd > /dev/null 2>&1
+echo "=> Done!"
